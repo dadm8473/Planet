6239a400f56e5db9169c82af8a56c68130d6d2d5
dadm8473 <dadm8473@gmail.com>
2019-07-31 16:24:12 +0900

    Move

---
+++ Assets/Script/MeteorSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int x = -180; x <= 180; ++x)
+            for(int y = -180; y <= 180; ++y)
+                for(int z = -180; z <= 180; ++z)
+                    Instantiate(cube, new Vector3(0, 0, -6), Quaternion.Euler(x, y, z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Transform maincamera;
+
+    void Start()
+    {
+        maincamera = Camera.main.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Rotate(v * 50 * Time.deltaTime, h * 50 * Time.deltaTime, 0f);
+
+        maincamera.transform.rotation = transform.rotation;
+    }
+}
