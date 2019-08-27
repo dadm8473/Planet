d06e032a628629a22e8d382f1c20736512a62911
dadm8473 <dadm8473@gmail.com>
2019-08-27 19:29:42 +0900

    Update

---
+++ Assets/Script/GameUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public void ReStart_Button()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit_Button()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
